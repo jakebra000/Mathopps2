e6c95a398bf2566c4b1ac724887cd0009129ddee
Brayden jake <jakebra000@warren.k12.in.us>
2024-08-29 09:39:00 -0400

    intial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+'''
+Write a program that takes your first name as user input
+Then ask the user for the number of repeats
+(Don't forget to convert this to an int)
+Then print out the first name joined to itself repeat number of times
+
+Sample Output if you used the word Nick for the name input and 3 for the repeat inputN
+NickNickNick
+'''
+name = input("Enter your first name: ")
+repeats=input("How many repeats do you want: ")
+repeats_num=int(repeats)
+
+repeats = input("How many repeats do you want: ")
+###YOUDO convert repeats to repeats_num with the int()
+repeats_num = int(repeats)
+print(name * repeats_num)
