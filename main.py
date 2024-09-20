ba967eaef478ea64dc48082c26c4202b8addd490
Brayden jake <jakebra000@warren.k12.in.us>
2024-09-20 09:32:54 -0400

    fixed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 frisbees = 3  #pretend we got from user input
 COST_OF_FRISBEES = 15
-total_cost =45 #YOU DO THIS PART WITH VARIABLES AND MATH OPS
+total_cost = frisbees * COST_OF_FRISBEES
 print("The total cost is $", total_cost, sep="")
-print("COST_OF_FRISBEES each  15$")
-print("I want to buy 3 FRISBEES" )
+
 
